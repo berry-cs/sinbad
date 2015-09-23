a51e45b7f619248849be3c879f3d6041dae5a478
Nadeem Abdul Hamid <nadeem@acm.org>
2015-09-23 14:28:47 -0400

    Created eclipse project

---
+++ src/java/sinbad/src/core/sig/ISig.java
@@ -0,0 +1,13 @@
+package core.sig;
+
+/**
+ * A signature approximately represents a Java type and is used
+ * to guide the instantiation of Java objects being bound to a 
+ * data source. 
+ * 
+ * @author Nadeem Abdul Hamid
+ *
+ */
+public interface ISig {
+
+}
