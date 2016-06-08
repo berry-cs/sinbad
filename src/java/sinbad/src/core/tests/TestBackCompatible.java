f5d4efdaab698752cbb5e9be830a72d89d683dea
Nadeem Abdul Hamid <nadeem@acm.org>
2016-06-08 08:32:40 -0400

    more compatibility tests

--- src/java/sinbad/src/core/tests/TestBackCompatible.java
+++ src/java/sinbad/src/core/tests/TestBackCompatible.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.json.*;
 import org.junit.Test;
 
@@ -36,7 +37,7 @@ public class TestBackCompatible {
     }
     
     @Test
-    public void testVehicles() {
+    public void testVehiclesXML() {
         DataSource ds = DataSource.connect("http://www.fueleconomy.gov/feg/epadata/vehicles.xml.zip");
         System.out.println("About to load...");
         
@@ -64,13 +65,90 @@ public class TestBackCompatible {
         }
         System.out.println("max mpg: " + max);
     }
-  /*  
-cannot unify core.tests.TestBackCompatible$Car{vehicle/make: <?>, vehicle/model: <?>, vehicle/city08: <?>} 
-with Car because 
-cannot unify core.tests.TestBackCompatible$Car{vehicle/make: <?>, vehicle/model: <?>, vehicle/city08: <?>} 
- with Car (scunify:unify-fail) (scunify:unify-fail/bc)
-*/
+ 
+    
+    @Test
+    public void testVehiclesCSV() {
+        DataSource ds = DataSource.connect("http://www.fueleconomy.gov/feg/epadata/vehicles.csv.zip");
+        System.out.println("About to load...");
+        
+        ds.load();
+        
+        System.out.println("Loaded!");
+        ds.printUsageString();
+        
+        System.out.println("Schema: " + ds.getDataAccess().getSchema().toString(true));
+        
+        Car c1 = ds.fetch("core.tests.TestBackCompatible$Car", "make", "model", "city08");
+        System.out.println("c1: " + c1);
 
+        ArrayList<Car> cs = ds.fetchList(Car.class, "make", "model", "city08");
+        System.out.println("size: " + cs.size());
+        Car max = cs.get(0);
+        for (Car c : cs) {
+            if (c.mpgCity > max.mpgCity) max = c;
+        }
+        System.out.println("max mpg: " + max);
+    }
+    
+    
+    
+    @Test
+    public void testAirport() {
+        DataSource ds = DataSource.connectAs("XML", "http://services.faa.gov/airport/status/ATL").set("format", "application/xml").setCacheTimeout(1).load();
+        ds.printUsageString();
+        APStatus x = ds.fetch("core.tests.APStatus", "Name", "State", "Delay", "Weather/Weather");
+        System.out.println(x);
+        System.out.println(ds.getFullPathURL());
+        System.out.println(ds.fetchString("Weather/Temp"));
+        System.out.println(ds.fetchString("Weather/Meta/Credit"));
+        System.out.println(ds.fetchString("Weather/Meta/Updated"));
+        System.out.println("cache timeout: " + ds.getCacheTimeout());
+        System.out.println("cache dir: " + ds.getCacheDirectory());
+    }
+    
+    @Test
+    public void testOpenFlights() {
+        DataSource ds = DataSource.connectAs("CSV", "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat");
+        ds.setOption("header", "\"ID\",\"Name\",\"Alias\",\"IATA\",\"ICAO\",\"Callsign\",\"Country\",Active");
+        ds.set("format", "raw");
+        ds.printUsageString();
+        ds.load();
+        ds.printUsageString();
+        //System.out.println("ds.size(): " + ds.size());
+        String[] names = ds.fetchStringArray("Name");
+        System.out.println(names.length + " airlines");
+        System.out.println(names[100]);
+        
+        /*
+        ds = DataSource.connect("src/easy/data/tests/dsspec2.xml").load();
+        ds.printUsageString();
+        names = ds.fetchStringArray("row/Name");
+        System.out.println(names.length + " airlines");
+        System.out.println(names[100]);
+        
+        System.out.println("---");
+        ds.getFieldSpec().apply(new FieldToXMLSpec()).write(new PrintWriter(System.out));
+        
+        ds = DataSource.connect("src/easy/data/tests/dsspec3.xml").load();
+        ds.printUsageString();
+        names = ds.fetchStringArray("Country");
+        System.out.println(IOUtil.join(ArrayUtils.subarray(names, 0, 20), ","));
+        */
+    }
+
+    
+    @Test
+    public void testTSV() {
+        DataSource ds = DataSource.connectAs("TSV", "http://data.beta.nyc//dataset/bdd4baaf-cba8-4727-ac21-41ece4dadc70/resource/56757db9-b46f-4e99-8c83-449d8ead8c69/download/tls-data.tsv");
+        ds.load();
+        ds.printUsageString();
+        String[] agencies = ds.fetchStringArray("ParticipatingAgency");
+        for (String a : agencies) {
+            System.out.println("   " + a);
+        }
+    }
+    
     
     //@Test
     public void testBartQuake() {
@@ -98,6 +176,25 @@ cannot unify core.tests.TestBackCompatible$Car{vehicle/make: <?>, vehicle/model:
     }
     
 
+}
+
+
+class APStatus {
+    String name;
+    String place;
+    boolean delay;
+    String weather;
+    
+    public APStatus(String name, String place, boolean delay, String weather) {
+        this.name = name;
+        this.place = place;
+        this.delay = delay;
+        this.weather = weather;
+    }
+    
+    public String toString() {
+        return name + " (" + place + ")" + (delay? " DELAY" : " no delay") + " / Weather: " + weather;
+    }
 }
 
 
