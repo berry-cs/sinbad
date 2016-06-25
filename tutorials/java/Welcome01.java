bdee59c4768806d8fa78324e072c62010b04ea32
Nadeem Abdul Hamid <nadeem@acm.org>
2016-06-25 09:06:16 -0400

    updating tutorials

---
+++ tutorials/java/Welcome01.java
@@ -0,0 +1,15 @@
+
+import core.data.*;
+
+public class Welcome01 {
+   public static void main(String[] args) {
+      String id = "KATL";
+      DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/" + id + ".xml"); 
+      ds.setCacheTimeout(15);  
+      ds.load();
+      //ds.printUsageString();
+      float temp = ds.fetchFloat("temp_f");
+      String loc = ds.fetchString("location");
+      System.out.println("The temperature at " + loc + " is " + temp + "F");
+   }
+}
---
+++ tutorials/java/Welcome01/Welcome01.pde
@@ -0,0 +1,14 @@
+import core.data.*; 
+
+void setup() {
+  DataSource.initializeProcessing(this);
+
+  String id = "KATL";
+  DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/" + id + ".xml"); 
+  ds.setCacheTimeout(15);  
+  ds.load();
+  //ds.printUsageString();
+  float temp = ds.fetchFloat("temp_f");
+  String loc = ds.fetchString("location");
+  System.out.println("The temperature at " + loc + " is " + temp + "F");
+}
