ddfe4181f16df07c1179df22c9bdffba9f0100f0
Nadeem Abdul Hamid <nadeem@acm.org>
2016-07-13 20:47:51 -0500

    remaining tutorials ported

---
+++ tutorials/java/Welcome02_Object.java
@@ -0,0 +1,54 @@
+
+import core.data.*;
+
+public class Welcome02_Object {
+   public static void main(String[] args) {
+      String id1 = "KATL";
+      DataSource ds1 = DataSource.connect("http://weather.gov/xml/current_obs/" + id1 + ".xml"); 
+      ds1.setCacheTimeout(15);  
+      ds1.load();
+      //ds1.printUsageString();
+
+      Observation ob1 = ds1.fetch("Observation", "weather", "temp_f", "wind_degrees");
+      System.out.println(id1 + ": " + ob1);
+      
+      String id2 = "KSAV";
+      DataSource ds2 = DataSource.connect("http://weather.gov/xml/current_obs/" + id2 + ".xml"); 
+      ds2.setCacheTimeout(15);  
+      ds2.load();
+      
+      Observation ob2 = ds2.fetch("Observation", "weather", "temp_f", "wind_degrees");
+      System.out.println(id2 + ": " + ob2);
+      
+      if (ob1.colderThan(ob2)) {
+         System.out.println("Colder at " + id1);
+      } else {
+         System.out.println("Colder at " + id2);
+      }
+   }
+}
+
+
+/* Represents a weather observation */
+class Observation {
+   float temp;    // in fahrenheit
+   int windDir;   // in degrees
+   String description;
+   
+   Observation(String description, float temp, int windDir) {
+      this.description = description;
+      this.temp = temp;
+      this.windDir = windDir;
+   }
+   
+   /* determine if the temperature of this observation is colder than 'that's */
+   public boolean colderThan(Observation that) {
+      return this.temp < that.temp;
+   }
+   
+   /* produce a string describing this observation */
+   public String toString() {
+      return (temp + " degrees; " + description + " (wind: " + windDir + " degrees)");
+   }
+}
+
