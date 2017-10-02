bc1f60c1cd5dd733cf3534d90532472658777ab3
Nadeem Abdul Hamid <nadeem@acm.org>
2017-10-02 10:52:56 -0400

    modify tutorial

---
+++ tutorials/java/Welcome03_List.java
@@ -0,0 +1,28 @@
+/*
+ * Arrays of objects
+ */
+
+import core.data.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Welcome03_List {
+   public static void main(String[] args) {
+      DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
+      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", 
+						       "station/station_id", "station/state",
+						       "station/latitude", "station/longitude");
+      System.out.println("Total stations: " + allstns.size());
+      
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a state abbreviation: ");
+      String state = sc.next();
+      System.out.println("Stations in " + state);
+      for (WeatherStation ws : allstns) {
+         if (ws.isLocatedInState(state)) {
+            System.out.println("  " + ws.getId() + ": " + ws.getName());
+         }
+      }
+   }
+}
+
